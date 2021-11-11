dbc8dbfa2a1633dc018496f512ca5c07cec45576
Autumn <blazeykirin@gmail.com>
2021-11-11 13:50:32 -0800

    add a function to make a tokio runtime

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,14 @@
 pub mod modules;
 pub mod env;
+
+pub fn make_tokio_runtime() -> tokio::runtime::Runtime {
+	let rt = tokio::runtime::Builder::new_multi_thread()
+		.enable_all()
+		.worker_threads(2)
+		.max_blocking_threads(32)
+		.thread_keep_alive(std::time::Duration::from_secs(60))
+		.build()
+		.unwrap();
+
+	rt
+}
