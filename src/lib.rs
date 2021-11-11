888c41917068b5db62b77a04f32214722776a24d
Autumn <blazeykirin@gmail.com>
2021-11-11 13:51:00 -0800

    add a prelude thingie should adjust as needed i think, i dunno if this is all useful or if i should add more stuff to it later

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,26 @@ pub fn make_tokio_runtime() -> tokio::runtime::Runtime {
 
 	rt
 }
+
+pub mod prelude {
+	// useful structs from this lib
+	pub use crate::env::Env;
+	pub use crate::modules::Event;
+	pub use crate::modules::InitStuff;
+
+	// useful structs and types from elsewhere
+	pub use std::sync::Arc;
+	pub use twilight_gateway::cluster::Cluster;
+	pub use twilight_gateway::cluster::Events;
+	pub use twilight_gateway::cluster::ShardScheme::Auto;
+	pub use twilight_gateway::Intents;
+
+	// useful traits
+	pub use futures::future::Future;
+	pub use futures::future::FutureExt;
+	pub use futures::stream::StreamExt;
+	pub use std::error::Error;
+
+	// useful functions
+	pub use tokio::spawn;
+}
