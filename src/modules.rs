6d7d541921e7210c3f989d484096e86bfd96be5c
Autumn Meadow <blazeykirin@gmail.com>
2021-11-13 01:40:43 +0000

    add a module handler to help with managing modules `Arc<Vec<Arc<Box<dyn Module>>>>` nice

--- src/modules.rs
+++ src/modules.rs
@@ -32,3 +32,47 @@ pub trait Module: Send + Sync {
 	async fn init(&mut self, _: &InitStuff) -> InitResult { Ok(()) }
 	async fn handle_event(&self, event: Event) -> HandleResult;
 }
+
+pub type ProperWrappedModules = Arc<Vec<Arc<Box<dyn Module>>>>;
+
+pub struct ModuleHandler {
+	modules: Vec<Box<dyn Module>>
+}
+
+impl ModuleHandler {
+	pub fn new() -> ModuleHandler {
+		ModuleHandler { modules: vec![] }
+	}
+
+	pub fn with_modules(modules: Vec<Box<dyn Module>>) -> ModuleHandler {
+		ModuleHandler { modules }
+	}
+
+	pub fn add_module(mut self, module: impl Module + 'static) -> ModuleHandler {
+		self.modules.push(Box::new(module));
+		self
+	}
+
+	pub fn add_modules(mut self, modules: Vec<Box<dyn Module>>) -> ModuleHandler {
+		self.modules.extend(modules);
+		self
+	}
+
+	pub async fn init_modules(mut self, current_user: CurrentUser, http: Arc<HttpClient>) -> crate::MainResult<ModuleHandler> {
+		let init_stuff = InitStuff { current_user, http };
+
+		for module in self.modules.iter_mut() {
+			module.init(&init_stuff).await?;
+		}
+
+		Ok(self)
+	}
+
+	pub fn into_modules(self) -> ProperWrappedModules {
+		Arc::new(
+			self.modules.into_iter()
+				.map(Arc::new)
+				.collect()
+		)
+	}
+}
