27ee7ad13c20eba60a4cb826936bab93e41247bf
Autumn Meadow <blazeykirin@gmail.com>
2021-11-14 02:23:59 +0000

    an env getter helper thing

--- src/env.rs
+++ src/env.rs
@@ -19,16 +19,14 @@ pub struct Env {
 
 impl Env {
 	pub fn get_env() -> Env {
-		use std::env::var;
-
 		init_dotenv();
 
-		let token = var("TOKEN")
-			.or_else(|_| var("BOT_TOKEN"))
-			.expect("could not find suitable bot token");
+		let token = env_var("TOKEN")
+			.or("BOT_TOKEN")
+			.get();
 
-		let port = var("PORT")
-			.unwrap_or_else(|_| "7079".into())
+		let port = env_var("PORT")
+			.get_or_default("7079".into())
 			.parse::<u16>()
 			.expect("failed to parse port");
 
@@ -62,4 +60,58 @@ impl Env {
 
 	#[inline]
 	pub fn development(&self) -> bool { false }
+}
+
+// struct that simplifies syntax of getting variables
+pub struct EnvVar {
+	var: Option<String>,
+	tried_names: Vec<String>
+}
+
+pub fn env_var(name: &str) -> EnvVar {
+	let var = std::env::var(name).ok();
+	EnvVar { var, tried_names: vec![name.into()] }
+}
+
+impl EnvVar {
+	pub fn or(mut self, name: &str) -> EnvVar {
+		if self.var.is_none() {
+			self.tried_names.push(name.into());
+			self.var = std::env::var(name).ok();
+		}
+		self
+	}
+
+	pub fn get(self) -> String {
+		// was this necessary? probably not.
+		// i leave it here just cause its kinda funny i suppose lol
+		// rust zero cost abstraction here? yes pls lol
+		// i would be glad if rust's implementation is faster than whatever
+		// woogly stuff this is
+
+		// match self.var {
+		// 	Some(var) => { var }
+		// 	None => {
+		//
+		// 		let mut buf_len = self.tried_names.len() * 4;
+		// 		self.tried_names.iter().for_each(|name| buf_len += name.len());
+		//
+		// 		let mut str_buf = String::with_capacity(buf_len);
+		// 		self.tried_names.iter().enumerate().for_each(|(i, name)| {
+		// 			if i > 0 { str_buf.push_str(", ") }
+		// 			str_buf.push('"');
+		// 			str_buf.push_str(name);
+		// 			str_buf.push('"');
+		// 		});
+		//
+		// 		panic!("could not find env variable. tried variables: {}", str_buf);
+		// 	}
+		// }
+
+		self.var.expect(&format!(r#"could not find a suitable environment variable. tried variables: "{}""#, self.tried_names.join(r#"", ""#)))
+	}
+
+	pub fn get_or_default(self, default: String) -> String {
+		self.var.unwrap_or(default.into())
+	}
 }
