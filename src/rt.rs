a52aa0c9fcb57e9bf279df08fd21a8841ced6203
Autumn Meadow <blazeykirin@gmail.com>
2022-01-06 11:51:16 -0800

    take advantage of more processing power
    num_cpus was already a dependency of a dependency of a dependency... so we can do this without bringing in another crate, so why not
    
    although for my purposes 2 threads is already overkill, but meh why not i guess lol

--- src/rt.rs
+++ src/rt.rs
@@ -1,9 +1,10 @@
 use tokio::runtime::Runtime;
+use std::cmp::max;
 
 pub fn make_tokio_runtime() -> Runtime {
 	let rt = tokio::runtime::Builder::new_multi_thread()
 		.enable_all()
-		.worker_threads(2)
+		.worker_threads(max(num_cpus::get(), 2))
 		.thread_keep_alive(std::time::Duration::from_secs(60))
 		.build()
 		.unwrap();
