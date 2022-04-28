fb4531168a18cc9838573ea4df91a44f07b4cc6a
Autumn Meadow <blazeykirin@gmail.com>
2022-04-28 16:00:05 -0700

    offer cheaper tokio runtime option

--- src/rt.rs
+++ src/rt.rs
@@ -1,11 +1,28 @@
+use std::time::Duration;
+use tokio::runtime::Builder;
 use tokio::runtime::Runtime;
 use crate::env::Env;
 
 pub fn make_tokio_runtime() -> Runtime {
-	let rt = tokio::runtime::Builder::new_multi_thread()
+	let rt = Builder::new_multi_thread()
 		.enable_all()
 		.worker_threads(Env::num_threads())
-		.thread_keep_alive(std::time::Duration::from_secs(60))
+		.thread_keep_alive(Duration::from_secs(60))
+		.build()
+		.unwrap();
+
+	rt
+}
+
+/// Does the same as [`make_tokio_runtme`] except it creates one using
+/// the current thread, which is more ideal for development environments
+/// where you don't need literally all of your computer's power
+///
+/// [`make_tokio_runtme`]: crate::rt::make_tokio_runtime
+pub fn make_cheap_tokio_runtime() -> Runtime {
+	let rt = Builder::new_current_thread()
+		.enable_all()
+		.thread_keep_alive(Duration::from_secs(60))
 		.build()
 		.unwrap();
 
